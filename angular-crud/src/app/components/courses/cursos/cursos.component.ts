ec7baaad2c69573ba55f35887cbc596d4d81bae2
Gustavo Paco <gustavopaco@gmail.com>
2023-08-21 11:44:38 -0300

    Adicionando arquivo JSON de cursos.
    Refatorando dados de cursos para receber em um observable.
    
    Adicionando delay na chamada de cursos, e inscricao somente na primeira chamada.
    
    Adicionando Spinner com tratamento de chamada assincrona.

--- angular-crud/src/app/components/courses/cursos/cursos.component.ts
+++ angular-crud/src/app/components/courses/cursos/cursos.component.ts
@@ -1,30 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatTableModule} from "@angular/material/table";
 import {Course} from "../../../shared/model/course";
 import {MatCardModule} from "@angular/material/card";
 import {CoursesService} from "../../../shared/services/courses.service";
+import {Observable, take} from "rxjs";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatCardModule],
+  imports: [CommonModule, MatTableModule, MatCardModule, MatProgressSpinnerModule],
   templateUrl: './cursos.component.html',
-  styles: []
+  styleUrls: ['../../../../assets/scss/cursos.scss']
 })
 export class CursosComponent implements OnInit {
-  courses: Course[] = [];
+  // destroyRef = inject(DestroyRef);
+  // courses: Course[] = [];
+  courses$: Observable<Course[]>
   columnsToDisplay = ['id', 'name', 'category', 'description'];
 
   constructor(private courseService: CoursesService) {
     // this.courses = [];
+      this.courses$ = this.courseService.getCourses();
   }
 
   ngOnInit(): void {
-    this.loadCourses();
+
   }
 
   private loadCourses(): void {
-    this.courses = this.courseService.getCourses();
+    // this.courseService.getCourses().pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+    //   next: data => this.courses = data,
+    //   error: err => console.log('Error', err)
+    // });
   }
 }
--- angular-crud/src/app/shared/services/courses.service.ts
+++ angular-crud/src/app/shared/services/courses.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import {HttpClient} from "@angular/common/http";
+import {delay, Observable, take, tap} from "rxjs";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = 'assets/data/courses.json'
+
   constructor(private request: HttpClient) { }
 
-  getCourses(): Course[]{
-    return [
-      {id: 1, name: 'Angular', category: 'Front-end', description: 'Curso de Angular 16'},
-      {id: 2, name: 'Java', category: 'Back-end', description: 'Curso de Java Avançado'},
-      {id: 3, name: 'Spring', category: 'Back-end', description: 'Curso de Spring Boot v3'},
-      {id: 4, name: 'React', category: 'Front-end', description: 'Curso de React JS'},
-      {id: 5, name: 'Vue', category: 'Front-end', description: 'Curso de Vue JS'},
-    ];
+  getCourses(): Observable<Course[]> {
+    return this.request.get<Course[]>(this.API)
+      .pipe(
+        take(1),
+        delay(2000),
+        tap(console.log)
+      );
+    // return [
+    //   {id: 1, name: 'Angular', category: 'Front-end', description: 'Curso de Angular 16'},
+    //   {id: 2, name: 'Java', category: 'Back-end', description: 'Curso de Java Avançado'},
+    //   {id: 3, name: 'Spring', category: 'Back-end', description: 'Curso de Spring Boot v3'},
+    //   {id: 4, name: 'React', category: 'Front-end', description: 'Curso de React JS'},
+    //   {id: 5, name: 'Vue', category: 'Front-end', description: 'Curso de Vue JS'},
+    // ];
   }
 }
