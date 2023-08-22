b9110af38d93c4ff16d401599ddc7124f438c26a
Gustavo Paco <gustavopaco@gmail.com>
2023-08-22 18:21:17 -0300

    Adicionando Bootstrap ao projeto. Adicionando pacote de classes SCSS Default ao projeto. Implementando parcialmente tela de formulario de cursos.

--- angular-crud/src/app/components/courses/curso-form/curso-form.component.ts
+++ angular-crud/src/app/components/courses/curso-form/curso-form.component.ts
@@ -1,31 +1,54 @@
 import {Component, DestroyRef, inject, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from "@angular/router";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-curso-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatToolbarModule, MatButtonModule],
   templateUrl: './curso-form.component.html',
-  styles: [
-  ]
+  styleUrls: ['../../../../assets/scss/curso-form.scss']
 })
 export class CursoFormComponent implements OnInit {
   params!: string;
   destroyRef = inject(DestroyRef);
+  formulario!: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.getParams();
-    console.log(this.params)
+    this.createForm();
   }
 
   private getParams(): void {
     this.activatedRoute.params.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(params => {
       this.params = params['id'];
     });
+  }
+
+  private createForm(): void {
+    this.formulario = this.fb.group({
+      id: [null, []],
+      name: [null, []],
+      description: [null, []],
+      courseCategory: this.fb.group({
+        id: [null, []],
+        name: [null, []],
+      })
+    });
+  }
+
+  onCancel() {
+
   }
 }
--- angular-crud/src/app/shared/model/course.ts
+++ angular-crud/src/app/shared/model/course.ts
@@ -3,7 +3,6 @@ import {CourseCategory} from "./courseCategory";
 export interface Course {
   id: number;
   name: string;
-  category: string;
   description: string;
   courseCategory: CourseCategory;
 }
